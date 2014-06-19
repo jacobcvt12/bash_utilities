63ede0f0a4f513a12154b71ecb52411ea02d2639
Jacob Carey <jacobcvt12@gmail.com>
2014-06-18 22:35:52 -0400

    Add several clarifying comments to main.cpp. Shift code around to be more readable. Remove unneeded code.

--- sample/src/main.cpp
+++ sample/src/main.cpp
@@ -4,20 +4,23 @@
 #include <unistd.h>
 #include "srs.hpp"
 
-// add in argument parsing
-// http://www.gnu.org/software/libc/manual/html_node/Example-of-Getopt.html
-
 int main(int argc, char* argv[])
 {
-    int sample_size;
-    std::string delimiter = "|"; // assume delimiter is pipe
-    int weight_field = 1; // assume field with weights is second field
-    std::string method = "srs"; // assume method is srs
+    /*
+     * Default values for arguments
+     */
 
-    int nflag = 0;
-    int hflag = 0;
+    std::string delimiter = "|"; 
+    int weight_field = 1; 
+    std::string method = "srs"; 
+
+    int sample_size;
+    int nflag = 0; // flag for sample size
+    int hflag = 0; // flag for header
 
     int c;
+
+    // Parse options passed via shell
 
     while ((c = getopt(argc, argv, "d:hm:n:w:")) != -1)
     switch(c)
@@ -47,62 +50,69 @@ int main(int argc, char* argv[])
 
     if (nflag == 0)
     {
+        // If user doesn't specify a sample size
+        // Return error and end
         std::cerr << "Must enter a sample size.\n";
 
         return 1;
     }
 
-    if (method != "srs" || method != "wrs")
+    if (method != "srs" && method != "wrs")
     {
+        // Method has to be srs (default) or wrs
         std::cerr << "Method must be srs or wrs.\n";
     }
 
 
-    std::istream* input;
-    std::ifstream infile;  
-    std::string input_device;
-    
-    // file piped to program
+    // pointer to input device
+    std::istream* input; 
+
     if (!isatty(fileno(stdin)))
     {
-        input = &std::cin;
-        input_device = "stdin";
+        // file piped to program
+        input = &std::cin; // pass reference to stdin to input
     }
 
-    // file not piped to program
     else
     {
-        int index;
-        opterr = 0;
-        int file_counter = 0; // there should only be one file. error if more
+        // file not piped to program
 
-        for (index = optind; index < argc; index++)
+        if (argc - optind > 1)
         {
-            file_counter++;
-            if (file_counter != 1)
-            {
-                std::cerr << "Must supply one and only one file.\n";
-                return 1;
-            }
+            // there should only be one file. error if more
 
-            infile.open(argv[index]);
-            input_device = argv[index];
-            input = &infile;
-
-            // check if argument is a file
-            if (!infile)
-            {
-                std::cerr << "File " << argv[index] << " doesn't exist or isn't open for reading.\n";
-                return 1;
-            }
-
+            std::cerr << "Must supply one and only one file.\n";
+            return 1;
         }
+
+        std::ifstream infile;  
+
+        // open file
+        infile.open(argv[optind]);
+
+        // pass pointer to file to input
+        input = &infile;
+
+        // check if argument is a file
+        if (!infile)
+        {
+            std::cerr << "File " << 
+                argv[optind] << 
+                " doesn't exist or isn't open for reading.\n";
+
+            return 1;
+        }
+
     }
     
+    // call srs function on input and store output in 
+    // std::vector sampled. Note, this means that resulting
+    // sample must be smalled enough to fit into memory! Even 
+    // though the initial data does not have to fit into
+    // memory and is read from disk.
     std::vector<std::string> sampled = srs(*input, sample_size, hflag);
 
-    std::cerr << " from " << input_device << std::endl;
-
+    // loop over sample and print to stdout
     for (auto it = sampled.begin(); it != sampled.end(); ++it)
     {
         std::cout << *it << std::endl;
